dba80426a911b5c92509c4d3fd3a84eaa8e6bce3
apillay023 <apillay023@student.wethinkcode.co.za>
2024-06-16 01:16:46 +0200

    My first GUI

--- Project_Managment_SystemJavaFX/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module org.example.project_managment_systemjavafx {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    requires com.dlsc.formsfx;
-
-    opens org.example.project_managment_systemjavafx to javafx.fxml;
-    exports org.example.project_managment_systemjavafx;
-}
