b6d7f41527baeac321d53b8625d12d8c3b5267fb
apillay023 <apillay023@student.wethinkcode.co.za>
2024-06-16 11:48:34 +0200

    Created classes for the Employee Project

---
+++ EmployeeManagementSystem/src/main/java/com/mycompany/employeemanagementsystem/EmployeeManagementSystem.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.employeemanagementsystem;
+
+/**
+ *
+ * @author ashley
+ */
+public class EmployeeManagementSystem {
+
+    public static void main(String[] args) {
+        new Menu().setVisible(true);
+    }
+}
